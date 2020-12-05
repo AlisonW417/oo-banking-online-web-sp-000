a3314c55e1a137ab6f63639e55eea16b9888a49f
Alison Williams <apw5011@gmail.com>
2020-12-05 23:51:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end 
   
   def execute_transaction
-    if @sender.balance < @amount 
+    if @sender.valid?
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     elsif @status == "pending"
