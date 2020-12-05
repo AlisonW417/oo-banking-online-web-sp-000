bfe32d19592958917e446375097b977184012abb
Alison Williams <apw5011@gmail.com>
2020-12-05 23:36:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   def execute_transaction
     if @sender.valid? == false 
       puts "Transaction rejected. Please check your account balance."
-    elsif self.status != "complete"
+    elsif self.status != "complete" && @sender.valid? == true 
       @sender.balance -= @amount 
       @receiver.balance += @amount 
       self.status = "complete"
